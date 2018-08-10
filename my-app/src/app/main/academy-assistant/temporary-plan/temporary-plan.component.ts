03dacbe09ccfb1a803914c4c27e6461bfa1bb19e
tngo35 <tngo35@csc.com>
2018-08-10 18:05:53 +0700

    temporaty-plan navi

--- my-app/src/app/main/academy-assistant/temporary-plan/temporary-plan.component.ts
+++ my-app/src/app/main/academy-assistant/temporary-plan/temporary-plan.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+import { MenuItem } from "primeng/api";
 @Component({
     selector: "app-temporary-plan",
     templateUrl: "./temporary-plan.component.html",
@@ -8,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class TemporaryPlanComponent implements OnInit {
     tmpCourses: any[] = [];
     constructor() {}
-
+    activeIndex: number = 0;
     ngOnInit() {
         this.getLocalStorage();
     }
@@ -16,4 +16,14 @@ export class TemporaryPlanComponent implements OnInit {
     getLocalStorage() {
         this.tmpCourses = JSON.parse(localStorage.getItem("tmp_courses"));
     }
+    items: MenuItem[] = [
+        {
+            label: "Kế Hoạch Đào Tạo",
+            command: event => {
+                this.activeIndex = 0;
+            }
+        }
+        
+    ];
+
 }
